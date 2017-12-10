95e7a9a88c61a4f852708b7b5c686e96ee92812c
zubernetes <zubernetes@gmail.com>
2017-12-10 16:30:25 -0500

    SpringBootApplications runs successfully w/ Gradle

--- src/main/kotlin/com/jogreen89/kotlinkalculator/KotlinKalculatorApplication.kt
+++ src/main/kotlin/com/jogreen89/kotlinkalculator/KotlinKalculatorApplication.kt
@@ -6,6 +6,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class KotlinKalculatorApplication
 
+class Adder(val args: Array<Int>) {
+  fun out() {
+    println("Running print() in Adder")
+  }
+}
+
+class Subtracter(val args: Array<Int>) {
+  fun out() {
+    println("Running Subtracter")
+  }
+}
+
+class Multiplier(val args: Array<Int>) {
+  fun out() {
+    println("Running Multiplier")
+  }
+}
+
+class Divider(val args: Array<Int>) {
+  fun out() {
+    println("Running Divider")
+  }
+}
+
 fun main(args: Array<String>) {
+  // Kotlin code is not running before SprintBootApplication
+  println("Kotlin Kalculator:")
+  if (args.size == 0) {
+    Message("Please provide a username to access the calculator.")
+    return
+  }
+  Message("Welcome to the Kalculator: ${args[0]}").out()
     SpringApplication.run(KotlinKalculatorApplication::class.java, *args)
+
 }
---
+++ src/main/kotlin/com/jogreen89/kotlinkalculator/Message.kt
@@ -0,0 +1,7 @@
+package com.jogreen89.kotlinkalculator
+
+class Message(val body: String) {
+  fun out() {
+    println("${body}")
+  }
+}
