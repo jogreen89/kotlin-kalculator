b1b6a927fbd4847353c3be8d3abbd0d99d8903e6
zubernetes <zubernetes@gmail.com>
2017-12-10 14:18:08 -0500

    Added Message and Adder classes

--- hello.kt
+++ hello.kt
@@ -1,13 +1,29 @@
 /**
  * Kotlin Kalculator
  *
- * Performs basic functions of a calculator
+ * Performs basic functions of a calculator. Entry point
  */
+
+class Message(val body: String) {
+  fun out() {
+    println("${body}")
+  }
+}
+
+class Adder(val args: Array<Int>) {
+  fun print() {
+    println("Running print() in Adder")
+    // Message("${}")
+  }
+}
+
+
 fun main(args: Array<String>) {
   println("Kotlin Kalculator:")
   if (args.size == 0) {
-    println("Please provide a username to access the calculator.")
+    Message("Please provide a username to access the calculator.")
     return
   }
-  print("Welcome to the Kalculator: ${args[0]}.\n")
+  // Adder(arrayOf(1, 2, 3)).print()
+  Message("Welcome to the Kalculator: ${args[0]}.").out()
 }
